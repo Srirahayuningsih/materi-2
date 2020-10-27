86a776fcd5920c26d500536a7e6a033c1aa16bac
Srirahayuningsih <71173889+Srirahayuningsih@users.noreply.github.com>
2020-10-27 15:18:11 +0700

    Initial commit

---
+++ system/resources/views/Informatika.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Halaman Jurusan Informatika</title>
+</head>
+<body>
+	<a href="{{url('Informatika')}}">Informatika></a>
+	<a href="{{url('Agroindustri')}}">Agroindustri></a>
+	<a href="{{url('Elektro')}}">Elektro></a>
+	<a href="{{url('Sipil')}}">Sipil></a>
+	<h2>Teknik Informatika</h2>
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis nesciunt illum quibusdam mollitia perferendis, sit sint, ipsum labore necessitatibus temporibus quod beatae reprehenderit fugiat assumenda commodi, voluptate. Exercitationem, deleniti ab eum perspiciatis doloribus distinctio id, doloremque aperiam a autem eos nemo, unde porro illo obcaecati nesciunt, dolores quas aut. Maiores.
+	</p>
+
+
+
+
+</body>
+</html>
---
+++ system/routes/web.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/Informatika', function() {
+	return view("Informatika");
+});
+
+Route::get('/Agroindustri', function() {
+	return view("Agroindustri");
+});
+
+Route::get('/Elektro', function() {
+	return view("Elektro");
+});
+
+Route::get('/Sipil', function() {
+	return view("Sipil");
+});
+
+Route::get('/contact', function() {
+	return view("contact");
+});
+
+Route::get('/register', function() {
+	return view("register");
+});
+
+Route::get('/about', function() {
+	return view("About");
+});
+
+Route::get('/home', function() {
+	return view("home");
+});
+
+Route::get('/detail', function() {
+	return view("detail");
+});
